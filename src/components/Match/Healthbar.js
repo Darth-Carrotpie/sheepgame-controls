98b90c358a7037471162e451ac2ddd593c06bf89
Vytautas <vytautas.vargonas@gmail.com>
2019-12-06 23:46:11 +0000

    make healthbar dynamic

--- src/components/Match/Healthbar.js
+++ src/components/Match/Healthbar.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import healthbar from '../../images/match/healthbar_background.png';
+import React from "react";
+import styled from "styled-components";
+
+import healthbar from "../../images/match/healthbar_background.png";
+import { useStateValue } from "../../store/StateContext";
+
+const ImageContainer = styled.div`
+  max-height: 100vh;
+  overflow: hidden;
+  position: absolute;
+  bottom: 0;
+`;
+
+const AbsoluteImage = styled.img`
+  max-height: 100vh;
+  margin-top: ${props => 100 - props.size}vh;
+`;
 
 function Healthbar() {
+  const [
+    {
+      match: { currentHealth }
+    }
+  ] = useStateValue();
   return (
-    <img
-      src={healthbar}
-      alt="healthbar"
-      style={{ maxWidth: '100%', maxHeight: '100vh', position: 'absolute' }}
-    />
+    <ImageContainer>
+      <AbsoluteImage
+        src={healthbar}
+        alt="healthbar"
+        size={currentHealth}
+      ></AbsoluteImage>
+      ;
+    </ImageContainer>
   );
 }
 
