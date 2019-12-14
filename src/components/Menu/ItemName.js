0f5c98dcb8405b6539b535fea725dc888256d1ba
Vytautas Vargonas <vytautas.vargonas@gmail.com>
2019-12-14 12:44:27 +0000

    fix text positioning

--- src/components/Menu/ItemName.js
+++ src/components/Menu/ItemName.js
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 import NerisBlack from '../../fonts/NerisBlack.otf';
 
 const Text = styled.div`
@@ -8,19 +8,20 @@ const Text = styled.div`
     src: url(${NerisBlack});
   }
   font-weight: bold;
-  font-size: ${props=>props.fontSize ? props.fontSize:35}px;
-  height: ${props=>props.height ? props.height:5}vh;
-/*   font-weight: 500; */
-  color:  ${props=>props.playerColor ? props.playerColor:"white"};
+  font-size: ${props => (props.fontSize ? props.fontSize : 35)}px;
+  height: ${props => (props.height ? props.height : 5)}vh;
+  /*   font-weight: 500; */
+  color: ${props => (props.playerColor ? props.playerColor : 'white')};
   max-width: 350px;
   width: 100%;
   display: flex;
   justify-content: center;
+  align-items: center;
 
-/*   background-color:orange;
+  /*   background-color:orange;
   opacity: 50%; */
 `;
 export function ItemName(props) {
-  return <Text{...props}>{props.nameValue ? props.nameValue:"noName"}</Text>;
+  return <Text {...props}>{props.nameValue ? props.nameValue : 'noName'}</Text>;
 }
 export default ItemName;
