0b81c390cb6aa4b81bf7e0f3e4368af731cc1122
Danielius <carrotpie@gmail.com>
2019-12-26 16:34:50 +0200

    build script

--- unity-build.sh
+++ unity-build.sh
@@ -3,6 +3,6 @@ echo "running script to change entry name and move the files"
 cd build
 mv index.html contr.html
 cd ..
-cp -r build C:\Users\carro\Desktop\Unity\Sheep Game\Assets\WebGLTemplates\AirConsole
-read -n 1 -s -r -p "Press any key to continue"
+cp -r build/* 'C:\Users\carro\Desktop\Unity\Sheep Game\Assets\WebGLTemplates\AirConsole\'
+# read -n 1 -s -r -p "Press any key to continue"
 #npm config set script-shell "C:\\Users\\carro\\AppData\\Local\\Atlassian\\SourceTree\\git_local\\git-bash.exe"
