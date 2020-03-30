487355d3f431b3235018298ed787dda08691d882
Vytautas Vargonas <vytautas.vargonas@gmail.com>
2020-03-30 15:37:01 +0100

    fix match bubble margin

--- src/components/Match/BubbleGroup.js
+++ src/components/Match/BubbleGroup.js
@@ -1,17 +1,19 @@
-import React from "react";
-import BigBubble from "./BigBubble";
-import SmallBubble from "./SmallBubble";
-import styled from "styled-components";
-import grassButton from "../../images/match/buy_grass_icon.png";
-import sheepButton from "../../images/match/buy_sheep_icon.png";
-import smiteButton from "../../images/match/smite_icon.png";
-import { useStateValue } from "../../store/StateContext";
-import upgradeIcons from "../../images/upgrade/icons";
+import React from 'react';
+import BigBubble from './BigBubble';
+import SmallBubble from './SmallBubble';
+import styled from 'styled-components';
+import grassButton from '../../images/match/buy_grass_icon.png';
+import sheepButton from '../../images/match/buy_sheep_icon.png';
+import smiteButton from '../../images/match/smite_icon.png';
+import { useStateValue } from '../../store/StateContext';
+import upgradeIcons from '../../images/upgrade/icons';
 
 const BubblesInLine = styled.div`
   flex: 1;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
+  margin-left: 17vw;
+  margin-right: 17vw;
   /*   background-color: gray;
   opacity: 50%; */
 `;
@@ -41,7 +43,7 @@ function ShowPriceVal(inputValue) {
   if (inputValue > 0) {
     return Math.round(inputValue * 10) / 10;
   } else {
-    return "";
+    return '';
   }
 }
 
@@ -53,14 +55,14 @@ function BubbleGroup() {
         <BigBubble
           bubbleImage={upgradeIcons[match.upgradeA_icon]}
           bckgColor={menu.playerColor}
-          onClick={() => SendMessage("upgrade1", match.upgradeA_price)}
+          onClick={() => SendMessage('upgrade1', match.upgradeA_price)}
         >
           {ShowPriceVal(match.upgradeA_price)}
         </BigBubble>
         <BigBubble
           bubbleImage={upgradeIcons[match.upgradeB_icon]}
           bckgColor={menu.playerColor}
-          onClick={() => SendMessage("upgrade2", match.upgradeB_price)}
+          onClick={() => SendMessage('upgrade2', match.upgradeB_price)}
         >
           {ShowPriceVal(match.upgradeB_price)}
         </BigBubble>
@@ -69,31 +71,31 @@ function BubbleGroup() {
         <SmallBubble
           bubbleImage={grassButton}
           top={2}
-          onClick={() => SendMessage("buyGrass", match.priceGrass)}
-          bckgColor={"#c2e5ac"}
+          onClick={() => SendMessage('buyGrass', match.priceGrass)}
+          bckgColor={'#c2e5ac'}
         >
-          {" "}
-          {Math.round(match.priceGrass * 10) / 10}{" "}
-        </SmallBubble>{" "}
+          {' '}
+          {Math.round(match.priceGrass * 10) / 10}{' '}
+        </SmallBubble>{' '}
         <SmallBubble
           bubbleImage={sheepButton}
           top="0"
-          onClick={() => SendMessage("buySheep", match.priceSheep)}
+          onClick={() => SendMessage('buySheep', match.priceSheep)}
           bckgColor={menu.playerColor}
         >
-          {" "}
-          {Math.round(match.priceSheep * 10) / 10}{" "}
-        </SmallBubble>{" "}
+          {' '}
+          {Math.round(match.priceSheep * 10) / 10}{' '}
+        </SmallBubble>{' '}
         <SmallBubble
           bubbleImage={smiteButton}
           top="2"
-          onClick={() => SendMessage("smash", match.priceSmash)}
-          bckgColor={"#f9b096"}
+          onClick={() => SendMessage('smash', match.priceSmash)}
+          bckgColor={'#f9b096'}
         >
-          {" "}
-          {Math.round(match.priceSmash * 10) / 10}{" "}
-        </SmallBubble>{" "}
-      </FloatingBubbles>{" "}
+          {' '}
+          {Math.round(match.priceSmash * 10) / 10}{' '}
+        </SmallBubble>{' '}
+      </FloatingBubbles>{' '}
     </div>
   );
 }
