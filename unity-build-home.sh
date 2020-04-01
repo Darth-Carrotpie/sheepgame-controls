bc4fd3ac283199c7fc28a0d7dacb5157b8ba6858
carrotpie <carrotpie@gmail.com>
2020-04-01 14:46:14 +0300

    fix react paths in package.json to build as relative

---
+++ unity-build-home.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "running script to change entry name and move the files"
+cd build
+mv index.html controller.html
+cd ..
+cp -r build/* 'C:\Users\carro\Documents\Unity Projects\Sheep Game\Assets\WebGLTemplates\AirConsole\'
+# read -n 1 -s -r -p "Press any key to continue"
+#npm config set script-shell "C:\\Users\\carro\\AppData\\Local\\Atlassian\\SourceTree\\git_local\\git-bash.exe"
