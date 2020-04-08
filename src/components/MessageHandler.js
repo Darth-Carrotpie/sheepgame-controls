6695f2e1e116e5099157fe5d3eaf359bb42cec9a
carrotpie <carrotpie@gmail.com>
2020-04-08 18:34:50 +0300

    confirmation of important messages

--- src/components/MessageHandler.js
+++ src/components/MessageHandler.js
@@ -1,24 +1,37 @@
 import { useEffect } from "react";
 import { useStateValue } from "../store/StateContext";
 
-function SendViewMessage(elementValue){
+function SendViewMessage(elementValue) {
   var data = { element: "view", value: elementValue };
-  console.log("sending view swiching confirmation: ",data);
+  console.log("sending view swiching confirmation: ", data);
   window.airconsole.message(window.airconsole.SCREEN, data);
 }
-
+function ReplyImportant(elementValue) {
+  var data = {
+    important: true,
+    token: elementValue["token"],
+    deviceId: elementValue["deviceId"],
+    element: "",
+    value: "",
+  };
+  window.airconsole.message(window.airconsole.SCREEN, data);
+}
 function MessageHandler({ children }) {
   const [, dispatch] = useStateValue();
   useEffect(() => {
-    window.airconsole.onMessage = function(from, data) {
-      if (from === window.AirConsole.SCREEN){
+    window.airconsole.onMessage = function (from, data) {
+      if (from === window.AirConsole.SCREEN) {
         if (data["type"] !== null && data["type"] !== "") {
-          if(data["type"] !== "playerProfile")
+          if (data["type"] !== "playerProfile")
             console.log("MessageHandler", from, data);
           dispatch(data);
         }
-      if(data["type"] === "changeView")
-        SendViewMessage(data["value"]);
+        if (data["type"] === "changeView") SendViewMessage(data["value"]);
+        if (data["important"] !== null) {
+          if (data["important"] === true) {
+            ReplyImportant(data);
+          }
+        }
       }
     };
   }, []);
--- unity-build-home.sh
+++ unity-build-home.sh
@@ -4,5 +4,6 @@ cd build
 mv index.html controller.html
 cd ..
 cp -r build/* 'C:\Users\carro\Documents\Unity Projects\Sheep Game\Assets\WebGLTemplates\AirConsole\'
+mv --force 'C:\Users\carro\Documents\Unity Projects\Sheep Game\Assets\WebGLTemplates\AirConsole\controller.html' 'C:\Users\carro\Documents\Unity Projects\Sheep Game\Assets\Scripts\Controller\controller.html'
 # read -n 1 -s -r -p "Press any key to continue"
 #npm config set script-shell "C:\\Users\\carro\\AppData\\Local\\Atlassian\\SourceTree\\git_local\\git-bash.exe"
