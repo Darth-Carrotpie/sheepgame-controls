a68e785bf8d7a87313040fe0154c3e068c410122
Vytautas Vargonas <vytautas.vargonas@gmail.com>
2020-10-08 22:55:21 +0100

    fix half bubbles

--- src/components/Match/BigBubble.js
+++ src/components/Match/BigBubble.js
@@ -1,19 +1,19 @@
-import React from "react";
-import styled from "styled-components";
-import BubbleText from "./BubbleText";
-import BubbleIcon from "./BubbleIcon";
+import React from 'react';
+import styled from 'styled-components';
+import BubbleText from './BubbleText';
+import BubbleIcon from './BubbleIcon';
 
 const BubbleButton = styled.button`
   height: 15vh;
   width: 15vh;
-  background-color: ${(props) => (props.bckgColor ? props.bckgColor : "#0f0")};
+  background-color: ${(props) => (props.bckgColor ? props.bckgColor : '#0f0')};
   display: inline-flex;
   position: relative;
   align-items: center;
   justify-content: center;
   margin-top: 0px;
   top: 0vh;
-  border: #f7931e ${(props) => (props.selected ? "3px solid" : "none")};
+  border: #f7931e ${(props) => (props.selected ? '3px solid' : 'none')};
   border-radius: 100%;
   padding: 0;
   &:active {
@@ -23,9 +23,16 @@ const BubbleButton = styled.button`
   :focus {
     outline: 0;
   }
-  opacity: ${(props) => (props.bubbleImage ? "100%" : " 30%")};
-  ${(props) => (props.rightHalf ? "margin-left: -7.5vh;" : "")}
-  ${(props) => (props.leftHalf ? "margin-right: -7.5vh;" : "")}
+  opacity: ${(props) => (props.bubbleImage ? '100%' : ' 30%')};
+`;
+
+const HalfBubble = styled(BubbleButton)`
+  border-radius: 0%;
+  ${(props) => (props.leftHalf ? 'border-top-left-radius: 7.5vh;' : '')}
+  ${(props) => (props.leftHalf ? 'border-bottom-left-radius: 7.5vh;' : '')}
+  ${(props) => (props.rightHalf ? 'border-top-right-radius: 7.5vh;' : '')}
+  ${(props) => (props.rightHalf ? 'border-bottom-right-radius: 7.5vh;' : '')}
+  width: 7.5vh;
 `;
 
 function BigBubble({ onClick, ...props }) {
@@ -39,11 +46,11 @@ function BigBubble({ onClick, ...props }) {
 
 export function BackBubble({ onClick, ...props }) {
   return (
-    <BubbleButton {...props} onClick={onClick}>
+    <HalfBubble {...props} onClick={onClick}>
       <BubbleText {...props} small>
         back
       </BubbleText>
-    </BubbleButton>
+    </HalfBubble>
   );
 }
 
--- src/components/Match/BubbleText.js
+++ src/components/Match/BubbleText.js
@@ -1,22 +1,20 @@
-import React from "react";
-import styled from "styled-components";
-import NerisBlack from "../../fonts/NerisBlack.otf";
+import React from 'react';
+import styled from 'styled-components';
+import NerisBlack from '../../fonts/NerisBlack.otf';
 
 const Text = styled.span`
   @font-face {
     font-family: NerisBlack;
     src: url(${NerisBlack});
   }
-  font-size: ${(props) => (props.small ? "1rem" : "1.9rem")};
+  font-size: ${(props) => (props.small ? '1rem' : '1.9rem')};
   font-weight: bold;
   font-family: NerisBlack;
   margin-top: 0%;
-  color: ${(props) => (props.textColor ? props.textColor : "white")};
+  color: ${(props) => (props.textColor ? props.textColor : 'white')};
   text-align: center;
   position: absolute;
-  ${(props) => (props.top ? "top: -0.5vh;" : "")};
-  ${(props) => (props.leftHalf ? "margin-left: -3.5vh;" : "")};
-  ${(props) => (props.rightHalf ? "margin-left: 3.5vh;" : "")};
+  ${(props) => (props.top ? 'top: -0.5vh;' : '')};
 `;
 
 function BubbleText(props) {
